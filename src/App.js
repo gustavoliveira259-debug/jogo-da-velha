ad9785ef23615b68f3a29e8e3be41029e00a40b8
gustavoliveira259-debug <gustavoliveira259@gmail.com>
2025-08-25 21:41:44 -0300

    FIX renomeamos o Square para Tabuleiro e criamos e usamos o conponente Square

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 //Criação do elemento isquere que ira compor o tabuleiro
 import "./App.css";
-export default function Squere(){
+
+
+function Square({valor}){
+return(
+<button className="Square">{valor}</button>
+)
+}
+
+
+
+export default function Tabuleiro(){
   return(
     <div>
       <div>
-        <button className= "squere">1</button>
-        <button className= "squere">2</button>
-        <button className= "squere">3</button>
+        <Square valor="1"/>
+        <Square valor="2"/>
+        <Square valor="3"/>
      </div>
      <div>
-        <button className= "squere">4</button>
-        <button className= "squere">5</button>
-        <button className= "squere">6</button>
+       <Square valor="4"/>
+        <Square valor="5"/>
+        <Square valor="6"/>
     </div>
     <div>
-        <button className= "squere">7</button>
-        <button className= "squere">8</button>
-        <button className= "squere">9</button>
+        <Square valor="7"/>
+        <Square valor="8"/>
+        <Square valor="9"/>
     </div>
     </div>
   )
