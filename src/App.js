44ba199fd36f909200b69431b5f62cc5c181e43a
gustavoliveira259-debug <gustavoliveira259@gmail.com>
2025-08-25 21:25:05 -0300

    NEW: Criação do tabuleiro 3x3

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import "./App.css";
 export default function Squere(){
   return(
     <div>
-    <button className= "squere">1</button>
-    <button className= "squere">2</button>
-    <button className= "squere">3</button>
-    <button className= "squere">4</button>
-    <button className= "squere">5</button>
-    <button className= "squere">6</button>
-    <button className= "squere">7</button>
-    <button className= "squere">8</button>
-    <button className= "squere">9</button>
+      <div>
+        <button className= "squere">1</button>
+        <button className= "squere">2</button>
+        <button className= "squere">3</button>
+     </div>
+     <div>
+        <button className= "squere">4</button>
+        <button className= "squere">5</button>
+        <button className= "squere">6</button>
+    </div>
+    <div>
+        <button className= "squere">7</button>
+        <button className= "squere">8</button>
+        <button className= "squere">9</button>
+    </div>
     </div>
   )
 }
